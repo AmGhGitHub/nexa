f48689f81432840c63fe49d8377cb3bcf08dfd3c
Amir <altghaderi@gmail.com>
2023-10-23 22:03:03 -0600

    Add pagination to table

--- app/emission-data/scope-1/page.tsx
+++ app/emission-data/scope-1/page.tsx
@@ -7,11 +7,10 @@ const page = async () => {
   const data = await axios
     .get("http://localhost:3000/api/emission-data/scope-1")
     .then((res) => res.data);
-  console.log(data);
 
   return (
     <div className="container mx-auto">
-      <h1 className="font-semibold text-xl text-orange-400">
+      <h1 className="mb-3 font-semibold text-xl text-emerald-700">
         Scope-1 Reference Data
       </h1>
       <DataTable columns={columns} data={data} />
