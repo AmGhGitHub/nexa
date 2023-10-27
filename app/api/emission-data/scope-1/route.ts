b99cafe56024204a48f2c3ea6bfdbbe73a7936af
Seyyed Ghaderi <sghaderi6@gatech.edu>
2023-10-27 12:19:44 -0600

    fix the issue with prisma

--- app/api/emission-data/scope-1/route.ts
+++ app/api/emission-data/scope-1/route.ts
@@ -1,9 +1,10 @@
-import { st_combus_hc_emis, st_combus_heat_content } from "@prisma/client";
 import prisma from "@/prisma/client";
+// import { st_combus_hc_emis, st_combus_heat_content } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
-  const st_combus_heat_cont = await prisma.st_combus_heat_content.findMany({});
+  const st_combus_heat_cont = await prisma.st_combus_hc.findMany({});
+  // console.log(st_combus_heat_cont);
   const st_combus_hc_emis = await prisma.st_combus_hc_emis.findMany({});
 
   // const st_combus_emis_hc_based = await prisma.st_Combus_Emis.findMany({
--- app/emission-data/scope-1/columns.tsx
+++ app/emission-data/scope-1/columns.tsx
@@ -1,17 +1,21 @@
 "use client";
 
-import { st_combus_heat_content, st_combus_hc_emis } from "@prisma/client";
+import { st_combus_hc, st_combus_hc_emis } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
-export const col_St_Combus_HeatContent: ColumnDef<st_combus_heat_content>[] = [
+export const col_St_Combus_HeatContent: ColumnDef<st_combus_hc>[] = [
   { accessorKey: "fuelType", header: "Fuel Type" },
-  { accessorKey: "fuelSubType", header: "Fuel SubType" },
-  { accessorKey: "heatContent", header: "Heat Content Value" },
-  { accessorKey: "unit", header: "Unit" },
+  { accessorKey: "fuelSubtype", header: "Fuel Sub-Type" },
+  { accessorKey: "hcValue", header: "HC Value" },
+  { accessorKey: "hcUnit", header: "HC Unit" },
 ];
 
 export const col_St_Combus_Emis: ColumnDef<st_combus_hc_emis>[] = [
   { accessorKey: "fuelType", header: "Fuel Type" },
-  { accessorKey: "fuelSubtype", header: "Fuel SubType" },
+  { accessorKey: "fuelSubtype", header: "Fuel Sub-Type" },
   { accessorKey: "CO2.emisValue", header: "CO2 Emis" },
   { accessorKey: "CO2.emisUnit", header: "CO2 Emis Unit" },
+  { accessorKey: "CH4.emisValue", header: "CH4 Emis" },
+  { accessorKey: "CH4.emisUnit", header: "CH4 Emis Unit" },
+  { accessorKey: "N2O.emisValue", header: "N2O Emis" },
+  { accessorKey: "N2O.emisUnit", header: "N2O Emis Unit" },
 ];
