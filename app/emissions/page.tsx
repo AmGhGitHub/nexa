93e475a336e1f1701deaf0b082f279fe66be9fa2
Amir <altghaderi@gmail.com>
2023-11-02 19:31:27 -0600

    change the client to prisma

--- app/emissions/page.tsx
+++ app/emissions/page.tsx
@@ -1,4 +1,7 @@
-"use client";
+// "use client";
+import axios from "axios";
+import prisma from "@/prisma/client";
+// import prisma from "@/prisma/client";
 
 import {
   colStCombusHCEmis,
@@ -18,23 +21,48 @@ import {
 import { DataTable } from "./data-table";
 
 const EmisFactorData = async () => {
-  const response = await fetch("/api/emissions");
-  const data = await response.json();
-  const {
-    st_combus_heat_cont,
-    st_combus_hc_emis,
-    st_combus_quant_emis,
-    gases_gwp,
-    blended_refrigerants_gwp,
-    electricity_us_emis,
-    electricity_canada_emis,
-    mb_combus_co2_emis,
-    mb_combus_ch4_n2o_emis,
-    scp3_cat4_upstream_trans_cat9_downstream_trans,
-    scp3_cat5_waste_ops_cat12_endlife_sold_prd,
-    scp3_cat6_busin_travel_cat7_emp_commute,
-    supply_chain_emis_price_based,
-  } = data;
+  // const response = await axios.get("http://localhost:3000/api/emissions");
+  // // const response = await axios.get("/api/emissions");
+  // const data = await response.data;
+  // const {
+  //   st_combus_heat_cont,
+  //   st_combus_hc_emis,
+  //   st_combus_quant_emis,
+  //   gases_gwp,
+  //   blended_refrigerants_gwp,
+  //   electricity_us_emis,
+  //   electricity_canada_emis,
+  //   mb_combus_co2_emis,
+  //   mb_combus_ch4_n2o_emis,
+  //   scp3_cat4_upstream_trans_cat9_downstream_trans,
+  //   scp3_cat5_waste_ops_cat12_endlife_sold_prd,
+  //   scp3_cat6_busin_travel_cat7_emp_commute,
+  //   supply_chain_emis_price_based,
+  // } = data;
+  const st_combus_heat_cont = await prisma.st_combus_hc.findMany({});
+  const st_combus_hc_emis = await prisma.st_combus_hc_emis.findMany({});
+  const st_combus_quant_emis = await prisma.st_combus_quant_emis.findMany({});
+  const mb_combus_co2_emis = await prisma.mb_combus_co2_emis.findMany({});
+  const mb_combus_ch4_n2o_emis = await prisma.mb_combus_ch4_n2o_emis.findMany(
+    {}
+  );
+  const gases_gwp = await prisma.gases_gwp.findMany({});
+  const blended_refrigerants_gwp =
+    await prisma.blended_refrigerants_gwp.findMany({});
+  const electricity_us_emis = await prisma.electricity_us_emis.findMany({});
+  const electricity_canada_emis = await prisma.electricity_canada_emis.findMany(
+    {}
+  );
+
+  const scp3_cat4_upstream_trans_cat9_downstream_trans =
+    await prisma.scp3_cat4_upstream_trans_cat9_downstream_trans.findMany({});
+  const scp3_cat5_waste_ops_cat12_endlife_sold_prd =
+    await prisma.scp3_cat5_waste_ops_cat12_endlife_sold_prd.findMany({});
+  const scp3_cat6_busin_travel_cat7_emp_commute =
+    await prisma.scp3_cat6_busin_travel_cat7_emp_commute.findMany({});
+
+  const supply_chain_emis_price_based =
+    await prisma.supply_chain_emis_price_based.findMany({});
 
   return (
     <div className="container mx-auto">
